604fde1922e9cf66d05c9a9451f563d1863f610c
cmichaels1209 <cm765@njit.edu>
2025-03-01 11:45:13 -0500

    Fixed errors

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,13 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, multiply, divide
 
 def test_addition():
     '''Test that addition function works '''
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
     '''Test that addition function works '''
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == 0
 
 def test_multiply():
     '''Test that addition function works '''
